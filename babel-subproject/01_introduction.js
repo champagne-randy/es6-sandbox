61ac14c2d5d9489bf26d21535235ea2c0de1f1d2
Randy Champagne <r-champagne@neiu.edu>
2016-04-01 17:04:24 -0400

    Removed babel-subproject

--- babel-subproject/01_introduction.js
+++
@@ -1,7 +0,0 @@
-/*
-Create a javascript program that takes the the first command-line argument and
-outputs it right after a "Hello " String using ES6 template strings.
-*/
-
-let name = process.argv[2]
-console.log(`Hello ${name}`)
